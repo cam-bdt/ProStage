04d21eae9329afeb3ace498e17d641574a046982
cam-bdt <camillebourdet1999@gmail.com>
2018-12-19 14:54:38 +0100

    ajout donnees faker dans la bdd

--- src/DataFixtures/ProStageFixtures.php
+++ src/DataFixtures/ProStageFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\Stage;
+use App\Entity\Formation;
+use App\Entity\Entreprise;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,12 +13,31 @@ class ProStageFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $visiteur = new Visiteur();
-        $visiteur->setFormation();
-        $visiteur->setPorte();
-        $visiteur->setAccompagnateur();
-        $manager->persist($visiteur);
+        //création générateur de données faker
+        $faker = \Faker\Factory::create('fr_FR'); // create a French faker
 
+        for($i=0;$i<6;$i++){
+        $entreprise = new Entreprise();
+        $entreprise->setNom($faker->company);
+        $entreprise->setAdresse($faker->address);
+        $entreprise->setActivite($faker->catchPhrase);
+        $entreprise->setSite($faker->domainName);
+        $manager->persist($entreprise);
+        
+        $formation = new Formation();
+        $formation->setLibelle($faker->jobTitle);
+        $manager->persist($formation);
+
+        $stage = new Stage();
+        $stage->setTitre($faker->jobTitle);
+        $stage->setDescription($faker->address);
+        $stage->setMail($faker->companyEmail);
+        $stage->setTelephone($faker->phoneNumber);
+        $stage->setNomContact($faker->name($gender = null));
+        $stage->setEntreprise($entreprise);
+        $stage->addFormation($formation);
+        $manager->persist($stage);
+        }
         $manager->flush();
     }
 }
