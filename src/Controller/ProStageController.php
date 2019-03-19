c2704ee8944378ff510f7ee27a42ad1402c5e00d
cam-bdt <camillebourdet1999@gmail.com>
2019-03-19 19:29:47 +0100

    ajout crud

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -238,16 +238,6 @@ class ProStageController extends AbstractController
         
     }
 
-    /**
-     * @Route("/formation/{id}",name="formation")
-     */
-    public function uneFormation($id)
-    {
-        $repositoryUneFormation = $this->getDoctrine()->getRepository(Formation::class)->find($id);
-        return $this->render('pro_stage/uneFormation.html.twig', [
-            'forma'=> $repositoryUneFormation,
-            'idFormation'=>$id]);
-    }
 
     /**
      * @Route("/recherche",name="Recherche")
